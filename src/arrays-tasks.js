21df294691a357322d997a2530edc4555a7350d7
DzmitryAlekseev <dmitry.alekseev.s@gmail.com>
2024-12-31 14:28:09 +0100

    feat: realization calculateBalance function (Tue, Dec 31, 2024 14:28:26 PM)

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -320,8 +320,10 @@ function selectMany(arr, childrenSelector) {
  *   calculateBalance([ [ 10, 8 ], [ 1, 5 ] ])  => (10 - 8) + (1 - 5) = 2 + -4 = -2
  *   calculateBalance([]) => 0
  */
-function calculateBalance(/* arr */) {
-  throw new Error('Not implemented');
+function calculateBalance(arr) {
+  return arr.length === 0
+    ? 0
+    : arr.reduce((sum, el) => sum + (el[0] - el[1]), 0);
 }
 
 /**
