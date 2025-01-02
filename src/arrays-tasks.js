e51df798b1672116c66768c46c3d4f153e089844
DzmitryAlekseev <dmitry.alekseev.s@gmail.com>
2025-01-02 14:02:39 +0100

    feat: realization getIndicesOfOddNumbers function (Thu, Jan 02, 2025 14:02:43 PM)

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -443,8 +443,10 @@ function getIdentityMatrix(n) {
  *    getIndicesOfOddNumbers([2, 4, 6, 8, 10]) => []
  *    getIndicesOfOddNumbers([11, 22, 33, 44, 55]) => [0, 2, 4]
  */
-function getIndicesOfOddNumbers(/* numbers */) {
-  throw new Error('Not implemented');
+function getIndicesOfOddNumbers(numbers) {
+  const arrOddIndex = [];
+  numbers.map((el, i) => (el % 2 !== 0 ? arrOddIndex.push(i) : el));
+  return arrOddIndex;
 }
 
 /**
