cb61c6fe06fdb08bb17b949a242743c35d13a126
DzmitryAlekseev <dmitry.alekseev.s@gmail.com>
2025-01-02 16:15:47 +0100

    feat: realization propagateItemsByPositionIndex function (Thu, Jan 02, 2025 16:15:44 PM)

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -541,8 +541,14 @@ function findLongestIncreasingSubsequence(nums) {
  *  propagateItemsByPositionIndex([ 'a', 'b', 'c', null ]) => [ 'a', 'b', 'b', 'c', 'c', 'c',  null, null, null, null ]
  *  propagateItemsByPositionIndex([ 1,2,3,4,5 ]) => [ 1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5 ]
  */
-function propagateItemsByPositionIndex(/* arr */) {
-  throw new Error('Not implemented');
+function propagateItemsByPositionIndex(arr) {
+  const result = [];
+  arr.map((el, i) => {
+    const nArr = new Array(i + 1).fill(el);
+    result.push(nArr);
+    return result;
+  });
+  return result.flat(Infinity);
 }
 
 /**
