e883b95e30edf3d69d0b7099ed98f5d455a5e128
DzmitryAlekseev <dmitry.alekseev.s@gmail.com>
2025-01-02 16:41:18 +0100

    feat: realization shiftArray function (Thu, Jan 02, 2025 16:41:21 PM)

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -564,8 +564,25 @@ function propagateItemsByPositionIndex(arr) {
  *    shiftArray(['a', 'b', 'c', 'd'], -1) => ['b', 'c', 'd', 'a']
  *    shiftArray([10, 20, 30, 40, 50], -3) => [40, 50, 10, 20, 30]
  */
-function shiftArray(/* arr, n */) {
-  throw new Error('Not implemented');
+function shiftArray(arr, n) {
+  const nArr = [];
+
+  if (n > 0) {
+    const leftPart = arr.slice(0, arr.length - n);
+
+    const rightPart = arr.slice(arr.length - n);
+    nArr.unshift(rightPart);
+    nArr.push(leftPart);
+  } else {
+    const leftPart = arr.slice(0, Math.abs(n));
+
+    const rightPart = arr.slice(leftPart.length, arr.length);
+
+    nArr.push(rightPart);
+    nArr.push(leftPart);
+  }
+
+  return nArr.flat(Infinity);
 }
 
 /**
